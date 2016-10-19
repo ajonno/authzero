2a8e64f1e85705c6051ef62b95d8117b3ad10088
Angus Johnston <ajonno68@gmail.com>
2016-10-19 18:28:25 +1100

    initial commit

---
+++ index.android.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+var Auth0Lock = require('react-native-lock');
+
+
+
+export default class authzero extends Component {
+
+  constructor(){
+    super();
+    var lock = new Auth0Lock({clientId: '0vKV0THvEkqEhvTSsqvLp0nDCcQN21Js', domain: 'aamsco.au.auth0.com'});
+    lock.show({}, (err, profile, token) => {
+      console.log('Logged in!');
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('authzero', () => authzero);
